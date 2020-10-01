54862282f58e78ff92cb3c485455450a25188157
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-09-30 20:32:46 -0600

    base server set up & running

---
+++ api/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const errorHandler = require('../api/errorhandler.js')
+const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
+
+// const usersRouter = require('../users/users-router.js');
+// const authRouter = require('../auth/auth-router.js')
+const server = express();
+
+const sessionConfig = {
+    name: 'spcookie',
+    secret: 'shh, this is a secret string',
+    cookie: {
+        maxAge: 60*60*1000, //1 hour
+        secure: false,
+        httpOnly: true,
+    },
+    resave: false,
+    saveUnitialized: false, //required for GDPR
+
+    store: new knexSessionStore({
+        knex: require('../database/db-config.js'),
+        tablename: 'sessions',
+        sidfieldname: 'sessionId',
+        createtable: true,
+        clearInterval: 60*60*1000
+    })
+}
+
+server.use(session(sessionConfig));
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// server.use('/api/users', usersRouter);
+// server.use('/api/', authRouter);
+
+server.get('/', (req, res) => {
+    res.json({ message: 'api is up and running'});
+})
+
+
+server.use(errorHandler);
+module.exports = server;
