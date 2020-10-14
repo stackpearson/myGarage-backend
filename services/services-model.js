5e840b79d095cc1094231c0b0ab6b4209caa0d0d
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-10-14 17:55:40 -0600

    updated findServices to also return the service id

--- services/services-model.js
+++ services/services-model.js
@@ -5,7 +5,7 @@ const db = require('../database/db-config.js');
 function findServices(id) {
     return db('user_services as us')
         .join('user_vehicles as uv', 'us.id', 'uv.service_id')
-        .select('uv.vehicle_id', 'us.service_name', 'us.service_date', 'us.service_mileage', 'us.next_service_date', 'us.next_service_mileage', 'us.service_notes')
+        .select('uv.vehicle_id', 'us.id', 'us.service_name', 'us.service_date', 'us.service_mileage', 'us.next_service_date', 'us.next_service_mileage', 'us.service_notes')
         .where('uv.user_id', '=', `${id}`)
 }
 
